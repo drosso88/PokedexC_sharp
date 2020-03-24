218828a2ce67f95f99fbd63f4dc321d162dd732c
rocio <rocio.soriano.gomez@gmail.com>
2020-03-24 20:29:34 +0100

    primer commit con el esqueleto inicial

---
+++ PokedexC_sharp/Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PokedexC_sharp
+{
+    class Conexion
+    {
+        public MySqlConnection conexion;
+
+        public Conexion()
+        {
+            conexion = new MySqlConnection("Server = 127.0.0.1; Database = listapokemons; Uid = root; Pwd =; Port = 3306");
+        }
+        // un obj DataTable es un array de dos dimensiones donde te devuelve la consulta
+        public DataTable getPokemonPorId(int id)
+        {
+            try
+            {
+                conexion.Open();
+                MySqlCommand consulta =
+                    //abrimos conexion, realizamos consulta, devuelve resulado que guarda en DataTable pokemons
+                    new MySqlCommand("SELECT * FROM pokemon WHERE id = '" + id + "'", conexion);
+                MySqlDataReader resultado = consulta.ExecuteReader();
+                DataTable pokemons = new DataTable();
+                pokemons.Load(resultado);
+                //cierra la conexion
+                conexion.Close();
+                return pokemons;
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
--- PokedexC_sharp/VentanaPrincipal.Designer.cs
+++ PokedexC_sharp/VentanaPrincipal.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.izq = new System.Windows.Forms.Button();
+            this.der = new System.Windows.Forms.Button();
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 368);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // izq
+            // 
+            this.izq.Location = new System.Drawing.Point(26, 416);
+            this.izq.Name = "izq";
+            this.izq.Size = new System.Drawing.Size(190, 95);
+            this.izq.TabIndex = 1;
+            this.izq.Text = "<";
+            this.izq.UseVisualStyleBackColor = true;
+            this.izq.Click += new System.EventHandler(this.izq_Click);
+            // 
+            // der
+            // 
+            this.der.Location = new System.Drawing.Point(254, 416);
+            this.der.Name = "der";
+            this.der.Size = new System.Drawing.Size(190, 95);
+            this.der.TabIndex = 2;
+            this.der.Text = ">";
+            this.der.UseVisualStyleBackColor = true;
+            this.der.Click += new System.EventHandler(this.der_Click);
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.Location = new System.Drawing.Point(469, 26);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(583, 63);
+            this.nombrePokemon.TabIndex = 3;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1090, 602);
+            this.Controls.Add(this.nombrePokemon);
+            this.Controls.Add(this.der);
+            this.Controls.Add(this.izq);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "VentanaPrincipal";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button izq;
+        private System.Windows.Forms.Button der;
+        private System.Windows.Forms.Label nombrePokemon;
     }
 }
 
